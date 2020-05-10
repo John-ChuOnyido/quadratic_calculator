9150fb3c76905fae414422c53df645af6ea68350
John-ChuOnyido <64579999+John-ChuOnyido@users.noreply.github.com>
2020-05-10 17:19:40 +0100

    Create Algebraic formulae.py

---
+++ Algebraic formulae.py
@@ -0,0 +1,46 @@
+#This program solves algebraic expression where the coefficient of x on the right is 0.
+def Johnchu():
+	y = -50
+	while y < 50:
+		if 2*y + 5 == 13:
+			print("Y =", y)
+		y += 1
+		#calling the function
+Johnchu()
+
+"""This program solves the quadratic equation of a kinds"""
+from math import sqrt #this imports square root form math library
+def quad():
+	X1 = (-b + sqrt(b**2-4*a*c))/(2*a)
+	X2 =(-b-sqrt(b**2-4*a*c))/(2*a)
+	print("The solution to the quadratic equation are ", X1, "and", X2)
+
+a = int(input("Please enter the value of a: " ))
+b = int(input("Please enter the value of b: " ))
+c = int(input("Please enter the value of c: " ))
+	
+quad()
+
+"""This program solves polynomial of power 3"""
+def John_Chu(x):
+	return x**3-3*x**2-6*x+8
+def Necessity():
+	x = -100
+	while x < 100:
+		if John_Chu(x) == 0:
+			print("Your solutions are :", x)
+		x += 1
+			
+Necessity()
+
+
+"""This program allows the user to solve algebraic expression this type: ax+b = cx+d"""
+def plugin():
+	x = (d-b)/(a-c)
+	return "The value of x is:",x
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
+d = int(input("Enter the value of d: "))
+#calling the function
+print(plugin())
