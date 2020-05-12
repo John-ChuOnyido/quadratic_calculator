d0104a3b8a57e046e0e56935a35264d68351a856
John-ChuOnyido <64579999+John-ChuOnyido@users.noreply.github.com>
2020-05-12 08:05:00 +0100

    Update Algebraic formulae.py

--- Algebraic formulae.py
+++ Algebraic formulae.py
@@ -1,12 +1,12 @@
 #This program solves algebraic expression where the coefficient of x on the right is 0.
-def Johnchu():
+def first_degree():
 	y = -50
 	while y < 50:
 		if 2*y + 5 == 13:
 			print("Y =", y)
 		y += 1
 		#calling the function
-Johnchu()
+first_degree()
 
 """This program solves the quadratic equation of a kinds"""
 from math import sqrt #this imports square root form math library
@@ -22,16 +22,16 @@ c = int(input("Please enter the value of c: " ))
 quad()
 
 """This program solves polynomial of power 3"""
-def John_Chu(x):
+def polynomial(x):
 	return x**3-3*x**2-6*x+8
-def Necessity():
+def poly_values():
 	x = -100
 	while x < 100:
 		if John_Chu(x) == 0:
 			print("Your solutions are :", x)
 		x += 1
 			
-Necessity()
+poly_values()
 
 
 """This program allows the user to solve algebraic expression this type: ax+b = cx+d"""
